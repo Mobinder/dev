773c54652d9487069820896003070c80ad14d400
nerwalm@msn.com <nerwalm@msn.com>
2017-07-13 13:57:04 +0100

    Initial changes in vs

---
+++ mvc/signalR/Chat1/Hubs/Startup.cs
@@ -0,0 +1,19 @@
+﻿using Owin;
+using Microsoft.Owin;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+[assembly: OwinStartup(typeof(Chat1.Hubs.Startup))]
+namespace Chat1.Hubs
+{
+
+        public class Startup
+        {
+            public void Configuration(IAppBuilder app)
+            {
+                app.MapSignalR();
+            }
+        }
+}
